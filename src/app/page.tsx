e1a01b24adbd6f70a442d892d8fc2d8d3f066ccd
barak.noam.haviv <barak.software.solutions@gmail.com>
2024-09-07 21:09:31 +0300

    remove signin and register links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import TodoList from "./components/TodoList";
 import RootLayout from "./layout";
 
@@ -6,10 +5,6 @@ export default function Home() {
   return (
     <RootLayout>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <nav>
-          <Link href="/register">Register</Link> |{" "}
-          <Link href="/signin">Sign In</Link>
-        </nav>
         <TodoList />
       </main>
     </RootLayout>
